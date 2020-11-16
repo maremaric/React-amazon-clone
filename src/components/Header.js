b678e26e87d56d6924a95eecbbf02fd65bb8b00e
maremaric <markoreus888@gmail.com>
2020-11-16 12:43:45 +0100

    vezba

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,12 @@ import "./Header.css";
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 
 function Header() {
+    const [{ basket }, dispatch] = useStateValue();
+
     return (
         <div className='header'>
             <Link to="/">
@@ -62,7 +65,7 @@ function Header() {
                         <ShoppingBasketIcon />
                         <span 
                             className="header__optionLineTwo header__basketCount">
-                            0
+                            {basket?.length}
                         </span>
                     </div>
                 </Link>
